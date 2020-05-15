404584a250b2e95b354f51aa7143d273f384c1f4
Anthony Passler <passleranthony@yahoo.com>
2020-05-15 14:43:24 +0100

    styling to the rest of the pages

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -16,10 +16,10 @@ class DosesController < ApplicationController
   end
 
   def destroy
-    @dose = Dose.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
     @dose.destroy
 
-    redirect_to cocktail_path(@dose)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
